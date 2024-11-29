09cbf2e280b94e11e74e000bb6837a9e1355268f
Muhammet Karakaş <dfdfgh524@hotmail.com>
2024-11-29 17:34:20 +0300

    Camera

--- Source/DungeonOfAtheris/Private/Characters/AuraCharacter.cpp
+++ Source/DungeonOfAtheris/Private/Characters/AuraCharacter.cpp
@@ -2,4 +2,33 @@
 
 
 #include "Characters/AuraCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	SpringArmComp=CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->TargetArmLength=300;
+	SpringArmComp->SetRelativeRotation(FRotator(0.0f, -45.0f, 0.0f));
+
+	CameraComp=CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp,USpringArmComponent::SocketName);
+
+	
+	SpringArmComp->bUsePawnControlRotation=false;
+	SpringArmComp->bEnableCameraLag=true;
+
+	SpringArmComp->bInheritPitch=false;
+	SpringArmComp->bInheritRoll=false;
+	SpringArmComp->bInheritYaw=false; 
+	
+	GetCharacterMovement()->bOrientRotationToMovement=true;
+	GetCharacterMovement()->bConstrainToPlane=true;
+	GetCharacterMovement()->bSnapToPlaneAtStart=true;
+	
+    bUseControllerRotationPitch=false;
+    bUseControllerRotationYaw=false;
+    bUseControllerRotationRoll=false;
+}
--- Source/DungeonOfAtheris/Public/Characters/AuraCharacter.h
+++ Source/DungeonOfAtheris/Public/Characters/AuraCharacter.h
@@ -6,12 +6,26 @@
 #include "Characters/BaseCharacter.h"
 #include "AuraCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class DUNGEONOFATHERIS_API AAuraCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
+
+	AAuraCharacter();
+
+public:
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Camera")
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Camera")
+	TObjectPtr<UCameraComponent> CameraComp;
+
+
+
+	
 	
 };
